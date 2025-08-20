4cae2bec538502f7625a541f49877cb8e3f6a805
dhananjay-yadav-it <dhananjayyadav1807@gmail.com>
2025-08-21 01:57:17 +0530

    Solve event Q

--- Eventp/event.js
+++ Eventp/event.js
@@ -9,3 +9,37 @@
 // Double click (ondblclick) event use karke ek box ka color red karo.
 
 // Keyboard press hone par console me key ka naam print karo.
+
+
+let changeColorDiv = document.querySelector("#change-color");
+changeColorDiv.addEventListener("mouseover", () => {
+    changeColorDiv.classList.add("bg-amber-600");
+    console.log("hye")
+})
+changeColorDiv.addEventListener("mouseout", () => {
+    changeColorDiv.classList.remove("bg-amber-600");
+})
+
+
+let form = document.querySelector("form");
+form.addEventListener("submit", () => {
+    alert("Form Submitted");
+})
+
+let inpPreview = document.querySelector("#input-preview");
+let previewBox = document.querySelector("#preview-box")
+console.dir(previewBox)
+inpPreview.addEventListener("input", () => {
+    previewBox.textContent = inpPreview.value;
+})
+
+let changeColorOnDbl = document.querySelector("#dblclick");
+changeColorOnDbl.addEventListener("dblclick", () => {
+    changeColorOnDbl.classList.add("bg-red-600");
+})
+
+let showKey = document.querySelector("#showKey");
+window.addEventListener("keydown", (event) => {
+    showKey.textContent = event.key;
+})
+
